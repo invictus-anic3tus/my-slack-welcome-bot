cd5b4e55aeb4de39634297ac9ffbfe3736c6641b
Anicetus <anic3tus.inv@gmail.com>
2026-05-12 15:44:23 -0500

    Update app.js again

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { App } from '@slack/bolt';
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
 app.event('member_joined_channel', async ({ event, client, say }) => {
